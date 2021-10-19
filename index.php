51af634177609bf85854bf784620eac02fb0e4cc
MilicaBukelic <76432857+MilicaBukelic@users.noreply.github.com>
2021-10-19 19:07:15 +0200

    Dodavanje novog foldera

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
+<?php
+
+    require "model/user.php";
+
+    session start();
+    if(isset($_POST['usrename'])  && isset($_POST['password'])){
+        $uname = isset($_POST['usrename']);
+        $upass = isset($_POST['password']);
+        $conn = new mysqli();
+        $korisnik = User(null,$uname, $upass);
+        //$odg = $korisnik->logInUser($uname, $upass, $conn);
+        $odg = User::logInUser($korisnik, $conn);
+        if($odg){
+            echo `
+            <script>       
+            console.log("Uspesno ste se prijavili");
+            </script>
+            `;
+
+            $_SESSION['user_id'] = $korisnik -> id;
+            header('Location: home.php')
+
+            exit();
+        }else{
+            echo `
+            <script>       
+            console.log("Uspesno ste se prijavili");
+            </script>
+            `;
+        }
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
